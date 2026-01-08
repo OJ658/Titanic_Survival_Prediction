23e8b04710b2e530e27181b74f03bdadac49511f
oj_yassine <ojyassine348@gmail.com>
2026-01-08 11:01:00 +0100

    prepare data

---
+++ survival_prediction.py
@@ -0,0 +1,11 @@
+#wrangle and cleanse the data
+import pandas as pd
+import numpy as np
+import random as rd
+
+#visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#modelling
+from sklearn.Linearmodel import LogisticRegression 
